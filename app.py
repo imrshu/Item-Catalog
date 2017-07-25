b4bed4113bfbb324547cdf02d81c905231f1ac62
imrshu <rishabh.verma11998@gmail.com>
2017-07-25 18:36:11 +0530

    Add neccessary modules

---
+++ app.py
@@ -0,0 +1,29 @@
+# Importing the database classes
+from db import Base, Category, Items
+
+# Importing necessary sqlalchemy modules
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# Importing neccessary flask modules
+from flask import Flask, render_tempalate, url_for, request, jsonify, flash, redirect
+
+# Creating instance of Flask 
+app = Flask(__name__, template_folder= 'templates')
+
+# Connect to the database
+engine = create_engine('sqlite:///catalog.db')
+
+# Bind engine to Base
+Base.metadata.bind = engine
+
+# Creating instance of sessionmaker
+DBSession = sessionmaker(bind=engine)
+
+# Instatiate the DBSession Class
+session = DBSession()
+
+
+if __name__ == '__main__':
+	app.secret_key= 'Super_secretKey'
+	app.run(host='0.0.0.0', port=8080, debug=True)
