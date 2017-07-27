38f24413832fd060644856a9a04c9e7cccb8b30d
imrshu <rishabh.verma11998@gmail.com>
2017-07-27 16:47:34 +0530

    Add User helper functions

--- app.py
+++ app.py
@@ -39,6 +39,30 @@ CLIENT_ID = json.loads(
     open('Client_Secrets/client_secrets.json', 'r').read())['web']['client_id']
 
 
+# User Helper Functions
+
+def createUser(login_session):
+    newUser = User(name=login_session['username'], email=login_session[
+                   'email'], picture=login_session['picture'])
+    session.add(newUser)
+    session.commit()
+    user = session.query(User).filter_by(email=login_session['email']).one()
+    return user.id
+
+
+def getUserInfo(user_id):
+    user = session.query(User).filter_by(id=user_id).one()
+    return user
+
+
+def getUserID(email):
+    try:
+        user = session.query(User).filter_by(email=email).one()
+        return user.id
+    except BaseException:
+        return None
+
+
 # Create an anti-forgery state token
 @app.route('/login')
 def showLogin():
